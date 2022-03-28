02a99a816ea13014b9756ada732936f7bcbccff6
Tim Malich <tim.malich@gmail.com>
2022-03-28 22:57:13 +0200

    correct none reproducible evil or example

--- sql_samples_not_reproducible/evil-or.sql
+++ sql_samples_not_reproducible/evil-or.sql
@@ -1,7 +1,4 @@
----------------------- NOTE -----------------------------
---
----------------------------------------------------------
--- BAD: With
+-- BAD:
 select *
 from sales
 where id = 10000
@@ -9,38 +6,22 @@ where id = 10000
    or id = 10002
    or id = 10003;
 
+-- GOOD:
 select *
 from sales
 where id in (10000, 10001, 10002, 100003);
 
-select outerUsers.uid,
-       outerUsers.givenname,
-       outerUsers.surname,
-       outerUsers.city,
-       outerSales.selling_price,
-       outerSales.payed
-from sales outerSales
-         join users outerUsers on outerSales.users_id = outerUsers.id
-where users_id in (
-    select innerUsers.id
-    from users innerUsers
-             join sales innerSales on innerUsers.id = innerSales.users_id
-             join products innerProducts on innerSales.products_id = innerProducts.id
-    where innerProducts.cost > 1000
-      and innerSales.payed < innerSales.selling_price
-      and outerUsers.id = outerSales.users_id
-)
-  and outerUsers.city = '10_city'
-;
 
-select distinct outerUsers.uid,
-                outerUsers.givenname,
-                outerUsers.surname,
-                outerUsers.city,
-                outerSales.selling_price,
-                outerSales.payed,
-                outerProducts.name,
-                outerShops.country_code
+---------------------- NOTE -----------------------------
+-- I tried hard to reproduce an evil or, but failed so far.
+-- I've seen them a couple of times in real and more complex queries. They usually appeared when we needed an `or`
+-- operation from a outer query within an exists clause. In some of those cases it was even faster to split the exists
+-- query at the `or` operation. However, I was not able to reproduce these with this dummy database.
+--
+-- I'll keep the example anyway, because the general message is: if you see a wild `or` be careful and take a closer look, it could be evil!
+---------------------------------------------------------
+-- BAD:
+select distinct outerUsers.*
 from sales outerSales
          join users outerUsers on outerSales.users_id = outerUsers.id
          join products outerProducts on outerSales.products_id = outerProducts.id
@@ -52,62 +33,10 @@ where exists(
                        join products innerProducts on innerSales.products_id = innerProducts.id
                        join shops innerShops on innerSales.shops_id = innerShops.id
               where innerProducts.cost > 1000
-                 or innerShops.country_code = 'DE'
+                 and (
+                     ---------------- THIS IS THE EVIL OR -----------------------------
+                     outerUsers.givenname like '%12%' or outerUsers.surname like '%12%'
+                     ------------------------------------------------------------------
+                     )
           )
-;
-
-select outerUsers.uid,
-                outerUsers.givenname,
-                outerUsers.surname,
-                outerUsers.city,
-                outerSales.selling_price,
-                outerSales.payed,
-                outerProducts.name,
-                outerShops.country_code
-from sales outerSales
-         join users outerUsers on outerSales.users_id = outerUsers.id
-         join products outerProducts on outerSales.products_id = outerProducts.id
-         join shops outerShops on outerShops.id = outerSales.shops_id
-where exists(
-              select innerUsers.id
-              from users innerUsers
-                       join sales innerSales on innerUsers.id = innerSales.users_id
-                       join products innerProducts on innerSales.products_id = innerProducts.id
-                       join shops innerShops on innerSales.shops_id = innerShops.id
-              where (innerProducts.cost > 1000
-                  or innerShops.country_code = 'DE'
-                        )
-                and outerSales.id = innerSales.id -- it's this back link that makes it currently bad, not the OR
-          )
-and outerSales.payed < 100;
-;
-
-select outerUsers.uid,
-       outerUsers.givenname,
-       outerUsers.surname,
-       outerUsers.city,
-       outerSales.selling_price,
-       outerSales.payed,
-       outerProducts.name,
-       outerShops.country_code
-from sales outerSales
-         join users outerUsers on outerSales.users_id = outerUsers.id
-         join products outerProducts on outerSales.products_id = outerProducts.id
-         join shops outerShops on outerShops.id = outerSales.shops_id
-where (exists(
-    select innerUsers.id
-        from users innerUsers
-                 join sales innerSales on innerUsers.id = innerSales.users_id
-                 join products innerProducts on innerSales.products_id = innerProducts.id
-        where innerProducts.cost > 1000
-    )
-  or
-    exists(
-            select innerUsers.id
-            from users innerUsers
-                     join sales innerSales on innerUsers.id = innerSales.users_id
-                     join shops innerShops on innerSales.shops_id = innerShops.id
-            where innerShops.country_code = 'DE'
-        ))
-  and outerUsers.uid = '23382_2uid';
 ;
